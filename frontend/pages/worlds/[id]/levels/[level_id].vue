e2d8374ce111aaf0c94912dae4023f8232e0d47b
Nicolas Huber <nicolas.huber2@uzh.ch>
2024-12-02 14:20:55 +0100

    Level hierarchy

---
+++ frontend/pages/worlds/[id]/levels/[level_id].vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{sdgId}}</p>
+    <p>{{levelId}}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRuntimeConfig } from "nuxt/app";
+
+definePageMeta({
+  middleware: ['level-guard'],
+});
+
+const route = useRoute();
+const router = useRouter();
+const config = useRuntimeConfig();
+
+const sdgId = route.params.id;
+const levelId = route.params.level_id;
+
+
+// Navigate back
+const goBack = () => {
+  router.push("/worlds");
+};
+</script>
+
+
+<style scoped>
+
+</style>
