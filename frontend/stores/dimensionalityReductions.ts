115f90fd44c94a25b01113cbc0e864c0d652c151
Nicolas Huber <nicolas.huber2@uzh.ch>
2024-12-14 10:52:49 +0100

    Refactoring

--- frontend/stores/dimensionalityReductions.ts
+++ frontend/stores/dimensionalityReductions.ts
@@ -1,10 +1,14 @@
-import { defineStore } from 'pinia';
-import { useRuntimeConfig } from 'nuxt/app';
-import { DimensionalityReductionGroupedResponse } from '~/types/dimensionalityReduction';
-import { CollectiveSummaryResponse } from "~/types/collectiveSummary"; // Define your response typ
+import { defineStore } from "pinia";
+import { useRuntimeConfig } from "nuxt/app";
+import {
+  type DimensionalityReductionGroupedResponse,
+  type DimensionalityReductionStats
+} from "~/types/dimensionalityReduction";
+import { type DimensionalityReduction } from "~/types/dimensionalityReduction";
+import { type CollectiveSummaryResponse } from "~/types/CollectiveSummaryResponse";
 
 
-export const useDimensionalityReductionsStore = defineStore('dimensionalityReductions', {
+export const useDimensionalityReductionsStore = defineStore("dimensionalityReductions", {
   state: () => ({
     reductions: {} as Record<
       number,
@@ -20,7 +24,7 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
     fetching: false,
     error: null as Error | null,
     userCoordinates: null as { x: number; y: number; z: number } | null, // User coordinates
-    selectedSummary: null as CollectiveSummaryResponse | null, // Add this field
+    selectedSummary: null as CollectiveSummaryResponse | null // Add this field
   }),
 
   getters: {
@@ -35,10 +39,10 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
 
       return levelData.reductions || null; // Return reductions for the level
     },
-    getSelectedPublicationsIds: (state) => ()  => {
+    getSelectedPublicationsIds: (state) => () => {
       return state.selectedPoints.forEach(point => {
-        point.publication_id
-      })
+        point.publication_id;
+      });
     }
   },
 
@@ -52,7 +56,7 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
     clearSelectedSummary() {
       this.selectedSummary = null;
     },
-    setSelectedPoints (points: any) {
+    setSelectedPoints(points: any) {
       this.selectedPoints = points;
     },
     async fetchUserCoordinates(query: string, sdg: number, level: number) {
@@ -64,16 +68,15 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
       this.error = null;
 
 
-
       try {
         const response = await $fetch<{ x: number; y: number; z: number }>(
           `${config.public.apiUrl}dimensionality_reductions/user-coordinates`,
           {
-            method: 'POST',
+            method: "POST",
             body: { user_query: query, sdg, level },
             headers: {
-              Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-            },
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`
+            }
           }
         );
 
@@ -107,10 +110,10 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
           $fetch<DimensionalityReductionGroupedResponse>(
             `${config.public.apiUrl}dimensionality_reductions?sdg=${sdgId}&level=${level}`,
             {
-              method: 'GET',
+              method: "GET",
               headers: {
-                Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-              },
+                Authorization: `Bearer ${localStorage.getItem("access_token")}`
+              }
             }
           )
         );
@@ -158,10 +161,10 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
         const result = await $fetch<DimensionalityReductionGroupedResponse>(
           `${config.public.apiUrl}dimensionality_reductions?${queryParams.toString()}`,
           {
-            method: 'GET',
+            method: "GET",
             headers: {
-              Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-            },
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`
+            }
           }
         );
 
@@ -199,7 +202,7 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
       const apiUrl = useRuntimeConfig().public.apiUrl;
 
       const data = {
-        publication_ids: this.selectedPoints.map(point => point.publication_id),
+        publication_ids: this.selectedPoints.map(point => point.publication_id)
       };
 
       try {
@@ -207,8 +210,8 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
           method: "POST",
           body: data,
           headers: {
-            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-          },
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          }
         });
 
         this.selectedSummary = response;
@@ -219,7 +222,7 @@ export const useDimensionalityReductionsStore = defineStore('dimensionalityReduc
       } finally {
         this.fetching = false; // Stop loading
       }
-    },
+    }
 
-  },
+  }
 });
