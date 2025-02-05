79ed1c74084811e15207e296eff14f043cd1ec9a
Nicolas Huber <nicolas.huber2@uzh.ch>
2025-02-05 17:03:31 +0100

    Layout

--- frontend/pages/labeling2/[publicationId].vue
+++ frontend/pages/labeling2/[publicationId].vue
@@ -1,26 +1,49 @@
 <template>
   <div class="flex flex-col h-screen">
-    <div class="grid grid-rows-10 grid-cols-8 grid-flow-col h-full">
-      <div class="row-span-1 col-span-2 bg-purple-400">
+    <div class="grid grid-rows-10 grid-cols-10 grid-flow-col h-full">
+      <div class="row-span-1 col-span-3 bg-purple-400">
         <SDGSelector></SDGSelector>
       </div>
-      <div class="row-span-9 col-span-2 bg-red-400">
+      <div class="row-span-9 col-span-3 bg-red-400">
         SHAP from Publication {{publicationId}}
         <ShapAbstract></ShapAbstract>
       </div>
-      <div class="row-span-10 col-span-2 bg-blue-400">
+      <div class="row-span-10 col-span-4 bg-blue-400">
         <AnnotationSection></AnnotationSection>
       </div>
-      <div class="row-span-1 col-span-2 bg-pink-400">
-        <SDGUserLabelToggle />
-        <BarLabelPlot :width="400" :height="100" />
+
+      <div class="row-span-1 col-span-3 bg-green-400">
+        <label for="content-toggle" class="mr-2 text-lg font-medium text-gray-700">Show User Labels</label>
+        <input
+          id="content-toggle"
+          type="checkbox"
+          class="toggle toggle-primary"
+          v-model="showContent"
+        />
       </div>
 
-      <div class="row-span-1 col-span-4 bg-yellow-400">
+
+
+      <div class="row-span-1 col-span-3">
+        <div class="grid grid-cols-3 gap-4">
+
+          <div v-if="showContent" class="row-span-1 col-span-2 bg-pink-400">
+            <SDGUserLabelToggle />
+            <BarLabelPlot :width="400" :height="100" />
+          </div>
+          <div  v-if="showContent" class="row-span-1 col-span-1 bg-green-400">
+            <QuestIndicator></QuestIndicator>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="showContent" class="row-span-1 col-span-3 bg-yellow-400">
         <CommentSummary></CommentSummary>
-
       </div>
-      <div class="row-span-8 col-span-4 bg-orange-400">
+
+
+
+      <div v-if="showContent" class="row-span-7 col-span-3 bg-orange-400">
         <!-- Toggle Switch -->
         <div class="flex justify-start items-center mb-4">
           <label for="comment-toggle" class="mr-2 text-lg font-medium text-gray-700">Show Annotations</label>
@@ -36,18 +59,9 @@
         <CommentSection v-if="!showAnnotations" />
         <CommentSectionAnnotations v-else />
      </div>
-     <div class="row-span-1 col-span-2 bg-green-400">
-       <QuestIndicator></QuestIndicator>
-       <UToggle
-         on-icon="i-heroicons-check-20-solid"
-         off-icon="i-heroicons-x-mark-20-solid"
-         :model-value="false"
-       />
-       <div class="badge badge-outline">default</div>
-       <div class="badge badge-primary badge-outline">primary</div>
-       <div class="badge badge-secondary badge-outline">secondary</div>
-       <div class="badge badge-accent badge-outline">accent</div>
-     </div>
+
+
+
    </div>
  </div>
 </template>
@@ -66,6 +80,7 @@ const route = useRoute()
 const publicationId = route.params.publicationId
 
 const showAnnotations = ref(false); // State to toggle between components
+const showContent = ref(false); // State to toggle the visibility of the sections
 
 </script>
 
